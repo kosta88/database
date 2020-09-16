96903d61deec2bde60428a2e5213aba7e972bcbb
kosta88 <39994278+kosta88@users.noreply.github.com>
2020-09-16 10:39:31 +0300

    update

--- src/routers/user.js
+++ src/routers/user.js
@@ -26,7 +26,7 @@ router.post('/users', async (req, res) => {
     const user = new User(req.body)
     try {
         await user.save()
-        sendWelcomeMail(user.email, user.name)          //can await cAUSE A PROMISE IS RETURNED
+        // sendWelcomeMail(user.email, user.name)          //can await cAUSE A PROMISE IS RETURNED
         const token = await user.generateAuthToken();
         res.status(201).send({ user, token })
     } catch (e) { res.status(400).send(e) }
@@ -92,7 +92,7 @@ router.delete('/users/me', auth, async (req, res) => {
     try {
         await req.user.removeTasks();
         await User.deleteOne({ _id: req.user._id })
-        sendCancelMail(req.user.email, req.user.name)
+        // sendCancelMail(req.user.email, req.user.name)
         res.send(req.user)
     } catch (e) { res.status(500).send() }
 })
@@ -103,7 +103,7 @@ router.post('/users/me/avatar', auth, upload.single('avatar'), async (req, res) 
     const bufferModifiedIMG = await sharp(req.file.buffer).resize({ width: 250, height: 250 }).png().toBuffer()
     req.user.avatar = bufferModifiedIMG
     await req.user.save()
-    res.send()
+    res.send()          //status 200
 }, (error, req, res, next) => {         //<<<<<   COMES AT THE END AND HAVE A CERTAIN CALL ARRANGMENT
     res.status(400).send({ error: error.message })
 })
@@ -118,9 +118,9 @@ router.delete('/users/me/avatar', auth, async (req, res) => {
 })
 
 
-router.get('/users/:id/avatar', async (req, res) => {
+router.get('/users/me/showAvatar', auth, async (req, res) => {
     try {
-        const user = await User.findById(req.params.id)
+        const user = await User.findById(req.user._id)
 
         if (!user || !user.avatar) {
             throw new Error()
