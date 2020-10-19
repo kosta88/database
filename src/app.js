471f8950114aa05fcff48f6f3c82eb48b8beec57
kosta88 <39994278+kosta88@users.noreply.github.com>
2020-10-19 12:41:54 +0300

    cors added

--- src/app.js
+++ src/app.js
@@ -1,12 +1,15 @@
 // const bcrypt = require('bcryptjs')                //has hash methods
 require('./db/mongoose')                           //make file run
 const express = require('express')
+
+const cors = require('cors')
 const userRouter = require('./routers/user')
 const taskRouter = require('./routers/task')
 
 const app = express()
-
 app.use(express.json())
+app.use(cors())
+
 app.use(userRouter)
 app.use(taskRouter)
 
